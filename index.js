e6bf884e36946e0c1ae0202000a46c41498399c0
Allan <mathuallan20@gmail.com>
2022-12-05 15:47:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const btn = document.getElementById("button")
+    btn.addEventListener("click",()=>{alert("Hello, I was clicked")})
 }
+addingEventListener()
+
+// const btn = document.getElementById('button')
+// btn.addEventListener('click', ()=> {alert("Hello, I was clicked")})
+// console.log
+
